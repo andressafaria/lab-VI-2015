ac09dc5c1a76158b6aca2e95219bbcd97b9e2b54
Andressa <andressa_faria18@hotmail.com>
2015-12-01 20:41:14 -0200

    Form -inserir
    Form- Inserir

--- SistemaFaq/admin/inserir.php
+++ SistemaFaq/admin/inserir.php
@@ -1,15 +1,18 @@
-<?php require_once '../header.php';?>
+<?php require_once 'header.php';?>
 
 <form action="javascript:void(0)">
-	<p>Inserir perguntas e respostas ao Faq</p>
-	<div>
-		<label for="pergunta">Pergunta:</label> <textarea rows="4" cols="50" id="pergunta"></textarea>
+	<div class="page-header">
+			<h1>Inserir perguntas e respostas ao Faq</h1>
 	</div>
 	<div>
-		<label for="resposta">Resposta:</label> <textarea rows="4" cols="50" id="resposta"></textarea>
+		<label for="pergunta">Pergunta:</label> <textarea required="required" rows="4" cols="50" id="pergunta"></textarea>
 	</div>
 	<div>
-		<button onclick="inserirFaq()">Inserir</button>
+		<label for="resposta">Resposta:</label> <textarea  required="required" rows="4" cols="50" id="resposta"></textarea>
+	</div>
+	<div>
+		<button class="btn btn-default" onclick="inserirFaq()">Inserir</button>
+		<button class="btn btn-default" onclick="window.history.back();">Voltar</button>
 	</div>
 	<span></span>
 </form>
