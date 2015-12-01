62f771109ecfc1ecefc3b8853a7a24fb760ccc1d
Jonathan <jonathanrs1@hotmail.com>
2015-12-01 19:29:10 -0200

    Index com bootstrap

--- SistemaFaq/header.php
+++ SistemaFaq/header.php
@@ -3,17 +3,45 @@
 	<meta charset="UTF-8">
 	<title>Sistema de FAQ</title>
 	
-	<link type="text/css" rel="stylesheet" href="require/css/style.css">
+	<!--<link type="text/css" rel="stylesheet" href="require/css/style.css"> -->
+	<link type="text/css" rel="stylesheet" href="require/css/bootstrap.min.css">
+	<link type="text/css" rel="stylesheet" href="require/font-awesome/css/font-awesome.min.css">
+	
+	<script type="text/javascript" src="require/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="require/js/jquery-1.10.2.min.js"></script>
 	<script type="text/javascript" src="require/js/login.js"></script>
+	<script type="text/javascript" src="require/js/listFaq.js"></script>
 	<script type="text/javascript" src="../require/js/faq.js"></script>
-	
+	<script type="text/javascript" src="../require/js/inserir.js"></script>
+
 </head>
 <body>
-	
-	<div id="dvBase">
-		<header>
-		
-		</header>
-		
-		<main>
+
+	<div class="container">
+
+		<nav class="navbar navbar-default navbar-fixed-top">
+			<div class="container">
+				<form class="navbar-form navbar-right" role="search"
+					action="javascript:void(0)">
+					<div class="form-group">
+
+
+
+
+
+						<input type="text" id="eLogin" class="form-control"	placeholder="Email"> 
+							<input type="password" id="sLogin" class="form-control" placeholder="Senha">
+
+
+						<button onclick="fctLogin()" class="btn btn-default">Entrar</button>
+				
+				</form>
+			</div>
+			</div>
+
+			
+			<span></span>
+	</nav>
+
+	<br>
+	<br>
